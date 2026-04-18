bc836ee935e8dbb4c9259d7a066ac3194d0ba210
Анна <plotnikovanana@gmail.com>
2026-04-18 10:27:25 +0300

    Step 3: Create WeatherData model

---
+++ app/src/main/java/com/plotnikova/weatherdashboard/data/WeatherData.kt
@@ -0,0 +1,7 @@
+data class WeatherData(
+    val temperature: Int? = null,
+    val humidity: Int? = null,
+    val windSpeed: Int? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
